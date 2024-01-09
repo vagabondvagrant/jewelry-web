14f38eb73bba0e085629042b3967db67df1fea0f
Kabeer Ahmad <kab6168@gmail.com>
2024-01-09 14:30:25 +0500

    kabeer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
-import Categories from "./Categories/Categories";
 import { CartProvider } from "./Context/CartContext";
 import { ContactUs } from "./Email/SendMail";
 import JewelryScroll from "./FramerMotion/JewelryScroll";
@@ -10,7 +8,6 @@ import Comment from "./Starreview/Comment";
 import WhatsappContact from "./Social/Whatsapp";
 import SignUpForm from "./SignUp/SignUp";
 import NavBar from './NavBar/NavBar';
-import About from './About'
 
 interface AppProps {}
 
@@ -25,40 +22,24 @@ const App: React.FC<AppProps> = () => {
 
   return (
     <CartProvider>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                {isLoggedIn ? (
-                  <>
-                    <NavBar />
-                    <JewelryScroll />
-                    <ContactUs />
-                    <StarReview />
-                    <div className="flex justify-center text-3xl mt-4 hover:bg-customcolor hover:text-white">
-                      {/* <SavedItems /> */}
-                    </div>
-                    <Comment />
-                    <WhatsappContact />
-                  </>
-                ) : (
-                  <SignUpForm onLogin={handleLogin} />
-                )}
-                <Outlet /> {/* This is where nested routes will be rendered */}
-              </div>
-            }
-          />
-          <Route
-            path="categories/*"
-            element={<Categories />}
-          >
-            <Route path="saved" element={<SavedItems />} />
-            <Route path="about" element={<About />} />
-          </Route>
-        </Routes>
-      </Router>
+      <div>
+        {isLoggedIn ? (
+          <>
+            <NavBar />
+            <JewelryScroll />
+            <ContactUs />
+            <SavedItems/>
+            <StarReview />
+            <div className="flex justify-center text-3xl mt-4 hover:bg-customcolor hover:text-white">
+              {/* <SavedItems /> */}
+            </div>
+            <Comment />
+            <WhatsappContact />
+          </>
+        ) : (
+          <SignUpForm onLogin={handleLogin} />
+        )}
+      </div>
     </CartProvider>
   );
 }
